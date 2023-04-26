5c7f05988eb3caa0af86316df698707273b10740
Jon Schwartz <jpschwartz2@uwalumni.com>
2023-04-26 15:05:06 -0700

    :wrench: Add example config.toml and instantiate in root

--- cmd/root.go
+++ cmd/root.go
@@ -19,6 +19,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/glif-confidential/cli/fevm"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -53,6 +54,9 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// Pulls in the FEVM connection params
+	fevm.InitFEVMConnection(rootCmd.Context())
 }
 
 // initConfig reads in config file and ENV variables if set.
