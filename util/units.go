5b8200338af37ce4133538943b6b1f74001ef5a4
Jonathan <jpschwartz2@uwalumni.com>
2023-05-07 21:18:10 -0700

    Fix unit conversion, add toatto func

--- util/units.go
+++ util/units.go
@@ -5,8 +5,17 @@ import (
 	"math/big"
 )
 
-func ToAtto(bal *big.Int) *big.Float {
+func ToAtto(bal *big.Float) *big.Int {
+	wei := new(big.Float).Mul(bal, big.NewFloat(math.Pow10(18)))
+	result := new(big.Int)
+	wei.Int(result)
+	return result
+}
+
+func ToFIL(atto *big.Int) *big.Float {
 	fbalance := new(big.Float)
-	fbalance.SetString(bal.String())
+	fbalance.SetString(atto.String())
 	return new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
 }
+
+var WAD = big.NewInt(1000000000000000000)
