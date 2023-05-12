f104e103ca74da756de05c7fd88d32b3df9d43ec
Jonathan <jpschwartz2@uwalumni.com>
2023-05-11 19:17:48 -0700

    Rename routerAddr in FEVMConnection struct

--- fevm/connect.go
+++ fevm/connect.go
@@ -16,7 +16,7 @@ type FEVMConnection struct {
 	LotusRpcUrl       string
 	EthRpcUrl         string
 	ChainID           int64
-	RouterAddress     common.Address
+	RouterAddr        common.Address
 	IFILAddr          common.Address
 	InfinityPoolAddr  common.Address
 	AgentFactoryAddr  common.Address
@@ -51,7 +51,7 @@ func InitFEVMConnection(ctx context.Context) error {
 		LotusRpcUrl:       ethRpcUrl,
 		EthRpcUrl:         ethRpcUrl,
 		ChainID:           chainID.Int64(),
-		RouterAddress:     common.HexToAddress(viper.GetString("routes.router")),
+		RouterAddr:        common.HexToAddress(viper.GetString("routes.router")),
 		IFILAddr:          common.HexToAddress(viper.GetString("routes.ifil")),
 		InfinityPoolAddr:  common.HexToAddress(viper.GetString("routes.infinity-pool")),
 		AgentFactoryAddr:  common.HexToAddress(viper.GetString("routes.agent-factory")),
