03109c1d8f86ec975758ec92fa1ccd08e3d08109
Jim Pick <jim@jimpick.com>
2023-07-27 15:11:36 -0700

    Migrate legacy keys in keys.toml to encrypted keystore

--- cmd/root.go
+++ cmd/root.go
@@ -111,6 +111,10 @@ func initConfig() {
 		logFatal(err)
 	}
 
+	if err := migrateLegacyKeys(); err != nil {
+		logFatal(err)
+	}
+
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
@@ -167,4 +171,40 @@ func initConfig() {
 		logFatalf("Failed to initialize pools sdk %s", err)
 	}
 	PoolsSDK = sdk
+}
+
+func migrateLegacyKeys() error {
+	if err := migrateLegacyKey(util.OwnerKey); err != nil {
+		return err
+	}
+	if err := migrateLegacyKey(util.OperatorKey); err != nil {
+		return err
+	}
+	return nil
+}
+
+func migrateLegacyKey(key util.KeyType) error {
+	ksLegacy := util.KeyStoreLegacy()
+	ks := util.KeyStore()
+	as := util.AgentStore()
+
+	pkStr, err := ksLegacy.Get(string(key))
+	if err != nil {
+		return err
+	}
+	if pkStr != "" {
+		pk, err := ksLegacy.GetPrivate(key)
+		if err != nil {
+			return err
+		}
+		account, err := ks.ImportECDSA(pk, "")
+		if err != nil {
+			return err
+		}
+		as.Set(string(key), account.Address.String())
+		ksLegacy.Set(string(key), "")
+		fmt.Printf("Migrated %s key to encrypted key store with empty passphrase.\n", key)
+	}
+	return nil
+
 }
