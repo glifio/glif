15b93ac725ebe9cb830119a9b50190b0b0882d0f
Jonathan <jpschwartz2@uwalumni.com>
2023-08-29 11:36:12 -0700

    Fix exit reserve command, update go -pools

--- cmd/infpool_exit_reserve.go
+++ cmd/infpool_exit_reserve.go
@@ -5,9 +5,11 @@ package cmd
 
 import (
 	"fmt"
+	"math/big"
 	"time"
 
 	"github.com/briandowns/spinner"
+	"github.com/glifio/go-pools/util"
 	"github.com/spf13/cobra"
 )
 
@@ -22,15 +24,43 @@ var exitReserveCmd = &cobra.Command{
 		s.Start()
 		defer s.Stop()
 
-		reserve, err := PoolsSDK.Query().InfPoolExitReserve(cmd.Context(), nil)
+		reserveBal, reserveMax, err := PoolsSDK.Query().InfPoolExitReserve(cmd.Context(), nil)
 		if err != nil {
 			logFatalf("Failed to get exit reserve %s", err)
 		}
 
 		s.Stop()
 
-		fmt.Printf("Total available liquidity in the Pool is %.08f FIL\n", reserve)
+		reserveBalFIL := util.ToFIL(reserveBal)
+
+		if reserveBal.Cmp(reserveMax) == 0 {
+			fmt.Printf("The exit reserve in the Pool is full at %.08f FIL\n", reserveBalFIL)
+		} else {
+			fmt.Printf("The exit reserve in the Pool is not full - it has %.08f FIL\n", reserveBalFIL)
+			// get the deficit
+			deficit := util.ToFIL(new(big.Int).Sub(reserveMax, reserveBal))
+			// div out the wads from the big ints
+			reserveBal.Div(reserveBal, big.NewInt(1e18))
+			reserveMax.Div(reserveMax, big.NewInt(1e18))
+			reservePerc := computePercentage(reserveBal, reserveMax)
+
+			fmt.Printf("The exit reserve is %.2f%% full, it needs %.08f FIL to be full\n", reservePerc, deficit)
+		}
 	},
+}
+
+// computePercentage computes (numerator/denominator) * 100
+func computePercentage(numerator, denominator *big.Int) float64 {
+	// Convert big.Int to big.Rat
+	rNumerator := new(big.Rat).SetInt(numerator)
+	rDenominator := new(big.Rat).SetInt(denominator)
+
+	// Compute the ratio
+	ratio := new(big.Rat).Quo(rNumerator, rDenominator)
+
+	percentageFloat := float64(ratio.Num().Int64()) / float64(ratio.Denom().Int64()) * 100
+
+	return percentageFloat
 }
 
 func init() {
