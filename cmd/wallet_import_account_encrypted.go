d35eab80f4ea3aba6ba9cc3677ca8de6dc9ff311
Jonathan <jpschwartz2@uwalumni.com>
2024-04-12 14:02:31 -0700

    Share logic between import account commands

--- cmd/wallet_import_account_encrypted.go
+++ cmd/wallet_import_account_encrypted.go
@@ -5,92 +5,43 @@ package cmd
 
 import (
 	"encoding/hex"
-	"errors"
-	"fmt"
-	"log"
-	"regexp"
 	"strings"
-	"time"
 
-	"github.com/AlecAivazis/survey/v2"
 	"github.com/glifio/cli/util"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 // importAccountCmd represents the import-account command
+// the validateImportKeyParams and completeImport functions are defined in the importAccountRawCmd
 var importAccountCmd = &cobra.Command{
 	Use:   "import-account [account-name] [account-encrypted-key-json]",
 	Short: "Import a single private key account, using an encrypted json key file",
 	Args:  cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
-		as := util.AccountsStore()
-
-		overWrite, err := cmd.Flags().GetBool("overwrite")
+		overwrite, err := cmd.Flags().GetBool("overwrite")
 		if err != nil {
 			logFatal(err)
 		}
 
 		name := strings.ToLower(args[0])
-		addrToOverwrite, err := as.Get(name)
-
-		rename := fmt.Sprintf("%s-replaced-%s", name, time.Now().Format(time.RFC3339))
-
-		var e *util.ErrKeyNotFound
-		if !errors.As(err, &e) && !overWrite {
-			logFatalf("Account %s already exists", name)
-		} else if !errors.As(err, &e) {
-			log.Printf("Warning: account '%s' already exists, renaming to '%s' and overriding with new '%s' key\n", name, rename, name)
-		} else if overWrite {
-			// here we dont actually have any keys to overwrite, so we set overWrite to false to avoid downstream issues
-			overWrite = false
-			log.Printf("Warning: no %s account to overwrite... importing %s\n", name, name)
-		} else {
-			log.Printf("Importing account: %s\n", name)
+		passphrase, addrToOverwrite, rename, err := validateImportKeyParams(name, overwrite)
+		if err != nil {
+			logFatal(err)
 		}
-
-		var passphrase string
-		var message = "Passphrase for account (or hit enter for no passphrase)"
-		prompt := &survey.Password{Message: message}
-		survey.AskOne(prompt, &passphrase)
-
-		re := regexp.MustCompile(`^[tf][0-9]`)
-		if strings.HasPrefix(name, "0x") || re.MatchString(name) {
-			logFatalf("Invalid name")
-		}
-
-		ks := util.KeyStore()
 
 		pkJSON, err := hex.DecodeString(args[1])
 		if err != nil {
-			logFatalf("Invalid private key hex string")
+			logFatalf("Invalid private key JSON file hex string")
 		}
 
-		account, err := ks.Import(pkJSON, passphrase, passphrase)
+		account, err := util.KeyStore().Import(pkJSON, passphrase, passphrase)
 		if err != nil {
 			logFatal(err)
 		}
 
-		// we rename the old named account to a new name so we dont lose a reference to this key
-		if overWrite {
-			as.Set(rename, addrToOverwrite)
-		}
-
-		as.Set(name, account.Address.String())
-
-		if err := viper.WriteConfig(); err != nil {
+		if err := completeImport(account.Address, name, rename, addrToOverwrite, overwrite); err != nil {
 			logFatal(err)
 		}
-
-		accountAddr, accountDelAddr, err := as.GetAddrs(name)
-		if err != nil {
-			logFatal(err)
-		}
-
-		bs := util.BackupsStore()
-		bs.Invalidate()
-
-		log.Printf("%s address: %s (ETH), %s (FIL) imported successfully\n", name, accountAddr, accountDelAddr)
 	},
 }
 
--- cmd/wallet_import_account_raw.go
+++ cmd/wallet_import_account_raw.go
@@ -12,11 +12,70 @@ import (
 	"time"
 
 	"github.com/AlecAivazis/survey/v2"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/glifio/cli/util"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
+
+func validateImportKeyParams(name string, overwrite bool) (string, string, string, error) {
+	as := util.AccountsStore()
+
+	addrToOverwrite, err := as.Get(name)
+
+	rename := fmt.Sprintf("%s-replaced-%s", name, time.Now().Format(time.RFC3339))
+
+	var e *util.ErrKeyNotFound
+	if !errors.As(err, &e) && !overwrite {
+		logFatalf("Account %s already exists", name)
+	} else if !errors.As(err, &e) {
+		log.Printf("Warning: account '%s' already exists, renaming to '%s' and overriding with new '%s' key\n", name, rename, name)
+	} else if overwrite {
+		// here we dont actually have any keys to overwrite, so we set overwrite to false to avoid downstream issues
+		overwrite = false
+		log.Printf("Warning: no %s account to overwrite... importing %s\n", name, name)
+	} else {
+		log.Printf("Importing account: %s\n", name)
+	}
+
+	var passphrase string
+	var message = "Passphrase for account (or hit enter for no passphrase)"
+	prompt := &survey.Password{Message: message}
+	survey.AskOne(prompt, &passphrase)
+
+	re := regexp.MustCompile(`^[tf][0-9]`)
+	if strings.HasPrefix(name, "0x") || re.MatchString(name) {
+		return passphrase, addrToOverwrite, rename, errors.New("Invalid name")
+	}
+	return passphrase, addrToOverwrite, rename, nil
+}
+
+func completeImport(address common.Address, name string, rename string, addrToOverwrite string, overwrite bool) error {
+	as := util.AccountsStore()
+
+	// we rename the old named account to a new name so we dont lose a reference to this key
+	if overwrite {
+		as.Set(rename, addrToOverwrite)
+	}
+
+	as.Set(name, address.String())
+
+	if err := viper.WriteConfig(); err != nil {
+		return err
+	}
+
+	accountAddr, accountDelAddr, err := as.GetAddrs(name)
+	if err != nil {
+		return err
+	}
+
+	bs := util.BackupsStore()
+	bs.Invalidate()
+
+	log.Printf("%s address: %s (ETH), %s (FIL) imported successfully\n", name, accountAddr, accountDelAddr)
+	return nil
+}
 
 // importAccountRawCmd represents the import-account command
 var importAccountRawCmd = &cobra.Command{
@@ -24,39 +83,15 @@ var importAccountRawCmd = &cobra.Command{
 	Short: "Import a single private key account, using a raw, unencrypted private key",
 	Args:  cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
-		as := util.AccountsStore()
-
-		overWrite, err := cmd.Flags().GetBool("overwrite")
+		overwrite, err := cmd.Flags().GetBool("overwrite")
 		if err != nil {
 			logFatal(err)
 		}
 
 		name := strings.ToLower(args[0])
-		addrToOverwrite, err := as.Get(name)
-
-		rename := fmt.Sprintf("%s-replaced-%s", name, time.Now().Format(time.RFC3339))
-
-		var e *util.ErrKeyNotFound
-		if !errors.As(err, &e) && !overWrite {
-			logFatalf("Account %s already exists", name)
-		} else if !errors.As(err, &e) {
-			log.Printf("Warning: account '%s' already exists, renaming to '%s' and overriding with new '%s' key\n", name, rename, name)
-		} else if overWrite {
-			// here we dont actually have any keys to overwrite, so we set overWrite to false to avoid downstream issues
-			overWrite = false
-			log.Printf("Warning: no %s account to overwrite... importing %s\n", name, name)
-		} else {
-			log.Printf("Importing account: %s\n", name)
-		}
-
-		var passphrase string
-		var message = "Passphrase for account (or hit enter for no passphrase)"
-		prompt := &survey.Password{Message: message}
-		survey.AskOne(prompt, &passphrase)
-
-		re := regexp.MustCompile(`^[tf][0-9]`)
-		if strings.HasPrefix(name, "0x") || re.MatchString(name) {
-			logFatalf("Invalid name")
+		passphrase, addrToOverwrite, rename, err := validateImportKeyParams(name, overwrite)
+		if err != nil {
+			logFatal(err)
 		}
 
 		pk := args[1]
@@ -65,33 +100,14 @@ var importAccountRawCmd = &cobra.Command{
 			logFatalf("Invalid private key")
 		}
 
-		ks := util.KeyStore()
-
-		account, err := ks.ImportECDSA(pkECDSA, passphrase)
+		account, err := util.KeyStore().ImportECDSA(pkECDSA, passphrase)
 		if err != nil {
 			logFatal(err)
 		}
 
-		// we rename the old named account to a new name so we dont lose a reference to this key
-		if overWrite {
-			as.Set(rename, addrToOverwrite)
-		}
-
-		as.Set(name, account.Address.String())
-
-		if err := viper.WriteConfig(); err != nil {
+		if err := completeImport(account.Address, name, rename, addrToOverwrite, overwrite); err != nil {
 			logFatal(err)
 		}
-
-		accountAddr, accountDelAddr, err := as.GetAddrs(name)
-		if err != nil {
-			logFatal(err)
-		}
-
-		bs := util.BackupsStore()
-		bs.Invalidate()
-
-		log.Printf("%s address: %s (ETH), %s (FIL) imported successfully\n", name, accountAddr, accountDelAddr)
 	},
 }
 
